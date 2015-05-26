729a4bc4b942089f793058e8f6c5aab4c9ca1490
Kirill Klenov <horneds@gmail.com>
2015-05-26 16:04:34 +0300

    Use user_info from aioauth lib

--- example/app.py
+++ example/app.py
@@ -25,21 +25,5 @@ def oauth(request):
     """ Oauth example. """
     provider = request.match_info.get('provider')
     client = yield from app.ps.oauth.login(provider, request)
-
-    if provider == 'bitbucket':
-        response = yield from client.request('GET', 'user')
-
-    elif provider == 'twitter':
-        response = yield from client.request('GET', 'account/verify_credentials.json')
-
-    elif provider == 'github':
-        response = yield from client.request('GET', 'user')
-
-    elif provider == 'facebook':
-        response = yield from client.request('GET', 'me')
-
-    elif provider == 'google':
-        response = yield from client.request('GET', 'people/me')
-
-    data = yield from response.json()
+    data = yield from client.user_info()
     return "<ul>%s</ul>" % "".join("<li><b>%s</b>: %s</li>" % item for item in data.items())
