12152bc519d42778b79e3d6ba4e07998f21ce2cf
Kirill Klenov <horneds@gmail.com>
2015-09-22 09:55:59 +0300

    Bump version: 0.2.2 → 0.2.3

--- muffin_oauth.py
+++ muffin_oauth.py
@@ -9,7 +9,7 @@ from muffin.plugins import BasePlugin
 from muffin_session import Plugin as SPlugin
 
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __project__ = "muffin-oauth"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
