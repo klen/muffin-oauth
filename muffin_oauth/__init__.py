32325aa5199d16c00d408b4acd5cd7f73bc3ac0c
Kirill Klenov <horneds@gmail.com>
2021-05-21 14:37:25 +0300

    build(version): 1.1.2 -> 2.0.0

--- muffin_oauth/__init__.py
+++ muffin_oauth/__init__.py
@@ -10,7 +10,7 @@ from muffin import Request, ResponseRedirect, ResponseError, Application
 from muffin.plugins import BasePlugin
 
 
-__version__ = "1.1.2"
+__version__ = "2.0.0"
 __project__ = "muffin-oauth"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
